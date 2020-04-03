7eaf161eb3e7aab86b9bcb4e1296ea72ec5e5a8a
THAVARAJAH Thanesh <thavarajah.thanesh@gmail.com>
2020-04-03 11:58:33 +0200

    implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,4 +1,41 @@
-
+/**
+ * 
+ * @author IN-DF-017
+ *
+ */
 public class Utilisateur {
 
-}
+	/*--------------props------------------------*/
+	
+	private int id;
+	private String nom;
+	
+	/*------------------ctor-----------------------*/
+	
+	public Utilisateur() {
+		
+		
+	}
+
+	
+	
+	/*--------------------get/set--------------------*/
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}//end classe
